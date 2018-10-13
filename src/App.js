6619d3af865e947c8677ee30860ae191f7f8236b
Fi1osof <n.lanets@modxclub.ru>
2018-10-13 03:30:37 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ export default class App extends PrismaApp {
     themeOptions: {
       direction: 'ltr',
       paletteType: 'light',
+      paletteType: 'dark',
     },
   }
 
--- src/components/Renderer/index.js
+++ src/components/Renderer/index.js
@@ -1,5 +1,7 @@
 
-import React from "react";
+import React, { Fragment } from "react";
+
+import PropTypes from "prop-types";
 
 import { Renderer as PrismaCmsRenderer } from "@prisma-cms/front";
 
@@ -7,9 +9,97 @@ import {
   ProductsRoutes,
 } from "../routes";
 
-export default class BoilerplateRenderer extends PrismaCmsRenderer {
 
+import PrismaCmsComponent from "@prisma-cms/component";
+
+import Paper from "material-ui/Paper";
+import Typography from "material-ui/Typography";
+import withStyles from "material-ui/styles/withStyles";
+
+
+export const styles = theme => {
+
+  console.log("theme", theme);
+
+  const {
+    palette: {
+      primary,
+    },
+  } = theme;
+
+  return {
+
+    root: {
+      color: "red",
   
+      "& a": {
+        color: primary[400],
+        textDecoration: "none",
+        
+        "&:hover": {
+          color: primary[600],
+        },
+      },
+  
+    },
+  }
+}
+
+class DemoAccess extends PrismaCmsComponent {
+
+  static contextTypes = {
+    ...PrismaCmsComponent.contextTypes,
+    openLoginForm: PropTypes.func.isRequired,
+  }
+
+  static propTypes = {
+    ...PrismaCmsComponent.propTypes,
+    classes: PropTypes.object.isRequired,
+  }
+
+  render() {
+
+    const {
+      openLoginForm,
+    } = this.context;
+
+    const {
+      classes,
+    } = this.props;
+
+    return <Paper
+      style={{
+        margin: 10,
+        padding: 10,
+      }}
+    >
+      <Typography
+        variant="title"
+      >
+        {this.lexicon("Use login demo and password for ")}
+        <a
+          href="javascript:;"
+          onClick={event => openLoginForm()}
+        >
+          <span
+          >
+            demo access
+          </span>
+        </a>
+      </Typography>
+    </Paper>
+
+  }
+}
+
+export class BoilerplateRenderer extends PrismaCmsRenderer {
+
+
+  static propTypes = {
+    ...PrismaCmsRenderer.propTypes,
+    classes: PropTypes.object.isRequired,
+  }
+
   getRoutes() {
 
     let routers = super.getRoutes();
@@ -38,5 +128,37 @@ export default class BoilerplateRenderer extends PrismaCmsRenderer {
     />;
   }
 
+  renderRoutes() {
+
+    const {
+      user: currentUser,
+    } = this.context;
+
+    const {
+      classes,
+    } = this.props;
+
+    const {
+      username,
+    } = currentUser || {}
+
+    return <div
+      className={classes.root}
+    >
+
+      {!currentUser || username !== "demo" ?
+        <DemoAccess
+          classes={classes}
+        />
+        :
+        null
+      }
+
+      {super.renderRoutes()}
+    </div>
+  }
+
 }
+
+export default withStyles(styles)(BoilerplateRenderer);
 
